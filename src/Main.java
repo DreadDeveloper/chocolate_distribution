563c63e11eea7a91e2ab8a6c5c80e10ce4615aea
Abhinav Kumar <kumaraka2000@gmail.com>
2023-04-16 13:19:25 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int[] arr = {12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 28, 42, 30, 44, 48, 43, 50};
+        int students = 7;
+        Solution solution = new Solution();
+        System.out.println(solution.distribution(arr, students));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Solution {
+import java.util.Arrays;
+public class Solution
+{
+    public int distribution(int[] arr, int students)
+    {
+        int minimum = Integer.MAX_VALUE;
+        Arrays.sort(arr);
+        for(int i = 0; i < arr.length - students + 1; i++)
+        {
+            if(arr[i + students - 1] - arr[i] < minimum)
+            {
+                minimum = arr[i + students - 1] - arr[i];
+            }
+        }
+        return minimum;
+    }
 }
